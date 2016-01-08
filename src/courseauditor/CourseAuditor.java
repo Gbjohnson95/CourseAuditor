9c74ead91b9c364c70d258cfbc5cb390e72fe046
Grant Johnson <gbjohnson@me.com>
2016-01-08 15:28:29 -0700

    Runs dates, cct audit, and ci audit now.

--- src/courseauditor/CourseAuditor.java
+++ src/courseauditor/CourseAuditor.java
@@ -13,7 +13,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
-import pageauditor.PageAuditor;
+import pageauditor.PageAuditorCCT;
+import pageauditor.PageAuditorCI;
 
 /**
  *
@@ -35,7 +36,8 @@ public class CourseAuditor {
             reports.mkdir();
         }
 
-        parseManifestAndRun("Reports/CourseAudit.csv");
+        parseManifestAndRunCI("Reports/CourseAuditCI.csv");
+        parseManifestAndRunCCT("Reports/CourseAuditCCT.csv");
         printDates("Reports/Dates.csv");
     }
 
@@ -93,12 +95,12 @@ public class CourseAuditor {
         }
     }
 
-    public static void parseManifestAndRun(String resultname) throws IOException {
+    public static void parseManifestAndRunCI(String resultname) throws IOException {
         String content = new Scanner(new File("imsmanifest.xml")).useDelimiter("\\Z").next();
         Document xmlDoc = Jsoup.parse(content, "", Parser.xmlParser());
         Elements resources = xmlDoc.select("resource");
         Elements items = xmlDoc.getElementsByTag("item");
-        String printString = "Title,HTML Title,Start Date,End Date,Due Date,Bad OUI,BH Links,Box Links,Benjamin Links,Empty Links,BH Images,Bolds,Bad Tags\n";
+        String printString = "Course Document Title,HTML Title,Wrong OrgUnitID,IL2 Links,Box Links,Benjamin Links,Empty Links,IL2 Images,CSS Bold,Potential Code Mistakes\n";
         //String printString = "Title,Benjamin Links,Course\n";
         Element manifest = xmlDoc.select("manifest").first();
 
@@ -107,19 +109,48 @@ public class CourseAuditor {
         //System.out.print("OrgUnitId: " + orgunitid);
         //*
         String date_start, date_end, date_due;
-        PageAuditor audit = new PageAuditor();
+        PageAuditorCI audit = new PageAuditorCI();
         for (Element e : resources) {
             String type = e.attr("d2l_2p0:material_type");
             if ("content".equals(type)) {
                 String fpath = e.attr("href");
                 for (Element d : items) {
                     if (d.hasAttr("identifierref") && (d.attr("identifierref").equals(e.attr("identifier")) && fpath.contains(".html"))) {
-                        date_start = d.attr("date_start");
-                        date_end = d.attr("date_end");
-                        date_due = d.attr("date_due");
                         //System.out.println(date_start + date_end + date_due);
                         title = d.child(0).ownText();
-                        audit.audit(fpath, title, orgunitid, date_start, date_end, date_due);
+                        audit.audit(fpath, title, orgunitid);
+                        printString += audit.getMetrics();
+                    }
+                }
+            }
+        }
+        try (PrintWriter out = new PrintWriter(resultname)) {
+            out.print(printString);
+        }
+    }
+
+    public static void parseManifestAndRunCCT(String resultname) throws IOException {
+        String content = new Scanner(new File("imsmanifest.xml")).useDelimiter("\\Z").next();
+        Document xmlDoc = Jsoup.parse(content, "", Parser.xmlParser());
+        Elements resources = xmlDoc.select("resource");
+        Elements items = xmlDoc.getElementsByTag("item");
+        String printString = "Title,HTML Title,Bad OUI,Calender Links,BH Links,Box Links,Benjamin Links,Bad Link Targets,Empty Links,BH Images,Image Width,Bolds,Spans,Bad Tags,Divs,Br,BHVars,Mentions Saturday,Header Order,Template,Filepath\n";
+        //String printString = "Title,Benjamin Links,Course\n";
+        Element manifest = xmlDoc.select("manifest").first();
+
+        String title;
+        String orgunitid = manifest.attr("identifier").substring(4);
+        //System.out.print("OrgUnitId: " + orgunitid);
+        //*
+        PageAuditorCCT audit = new PageAuditorCCT();
+        for (Element e : resources) {
+            String type = e.attr("d2l_2p0:material_type");
+            if ("content".equals(type)) {
+                String fpath = e.attr("href");
+                for (Element d : items) {
+                    if (d.hasAttr("identifierref") && (d.attr("identifierref").equals(e.attr("identifier")) && fpath.contains(".html"))) {
+                        title = d.child(0).ownText();
+                        audit.audit(fpath, title, orgunitid);
                         printString += audit.getMetrics();
                     }
                 }
---
+++ src/pageauditor/PageAuditorCCT.java
@@ -0,0 +1,265 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pageauditor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author gbjohnson
+ */
+public class PageAuditorCCT {
+
+    private Document doc;
+    private Element body;
+    private String filepath, docTitle, htmlString, oui;
+    private Elements bs, is, images, spans, divs, titleE, brs, links;
+
+    public void audit(String filename, String dTitle, String orgunitid) throws IOException {
+        File input = new File(filename);
+        oui = orgunitid;
+        if (input.exists()) {
+            doc = Jsoup.parse(input, "UTF-8");
+            body = doc.getElementsByTag("body").first();
+
+            // Set elements for program
+            links = body.getElementsByTag("a");
+            images = body.getElementsByTag("img");
+            spans = body.getElementsByTag("span");
+            divs = body.getElementsByTag("div");
+            is = body.getElementsByTag("i");
+            bs = body.getElementsByTag("b");
+            brs = body.getElementsByTag("br");
+            htmlString = doc.toString();
+            titleE = doc.select("title");
+
+            // Remove commas from the titles
+            docTitle = dTitle.replace(",", "");
+            docTitle = docTitle.replaceAll("\t", "");
+            docTitle = docTitle.replaceAll("\n", "");
+            filepath = filename.replace(",", "");
+        }
+    }
+
+    /**
+     * Returns the string with the metrics.
+     *
+     * @return
+     */
+    public String getMetrics() {
+        String printString
+                = docTitle + "," // Title
+
+                + getHTMLTitle() + "," // HTML Title
+                + wrongcourselinks() + "," // Links pointing outside of course
+                + callinks() + "," // Calender links
+                + numBHLinks() + "," // BH Links
+                + numBXLinks() + "," // Box Links
+                + benjaminLinks() + "," // Benjamin Links
+                + numBadTargets() + "," // Bad Link Targets
+                + numEmptyLinks() + "," // Empty Links
+                + numBHImages() + "," // BH Images
+                + numBadImageWidth() + "," // Image Width
+                + numBolds() + "," // Bolds
+                + spans.size() + "," // Spans
+                + (bs.size() + is.size()) + "," // Bad Tags
+                + numDivs() + "," // Divs
+                + brs.size() + "," // Br
+                + countBHVars() + "," // BHVars
+                + mentionsSaturday() + "," // Mentions Saturday
+                + checkHeaders() + "," // Headers
+                + getTemplateName() + "," // Template
+                + checkFilePath() + ",\n";  // File Path
+
+        return printString;
+    }
+
+    private String checkFilePath() {
+        if (filepath.contains("Course Files") || filepath.contains("Content Files") ) {
+            return "Good: " + filepath;
+        } else {
+            return "Bad: " + filepath;
+        }
+    }
+
+    private String benjaminLinks() {
+        int bhlinksCounter = 0;
+        bhlinksCounter = links.stream().map((a) -> a.attr("href")).filter((href) -> (href.toLowerCase().contains("courses.byui.edu"))).map((_item) -> 1).reduce(bhlinksCounter, Integer::sum);
+        return bhlinksCounter + "";
+    }
+    
+    private String wrongcourselinks() {
+        int wronglinkscounter = 0;
+        for (Element a : links) {
+            if (a.attr("href").contains("/d2l/") && !a.attr("href").contains(oui)) {
+                wronglinkscounter++;
+            }
+        }
+        return wronglinkscounter + "";
+    }
+    
+    
+    private String callinks() {
+        int callinkscounter = 0;
+        for (Element a : links) {
+            if (a.attr("href").contains("/d2l/le/calendar/")) {
+                callinkscounter++;
+            }
+        }
+        return callinkscounter + "";
+    }
+    
+    private String getHTMLTitle() {
+        if (titleE.isEmpty()) {
+            return "ERROR: COULD NOT READ TITLE";
+        } else {
+            String title = titleE.first().text().replace(",", "");
+            title = title.replaceAll("\t", "");
+            title = title.replaceAll("\n", "");
+            if (title == null ? docTitle == null : title.toLowerCase().trim().equals(docTitle.trim().toLowerCase())) {
+                return "Matching";
+            } else {
+                return title;
+            }
+        }
+    }
+
+    private String getTemplateName() {
+        String returnString = "";
+        for (Element img : images) {
+            if (img.attr("alt").toLowerCase().contains("banner")) {
+                if (img.attr("src").contains("largeBanner")) {
+                    returnString = "Large";
+                }
+                if (img.attr("src").contains("smallBanner")) {
+                    returnString = "Small";
+                }
+            }
+        }
+        return returnString;
+    }
+
+    private String mentionsSaturday() {
+        String returnString = "No";
+        Pattern dueSaturday = Pattern.compile("[sS]aturday");
+        Matcher m = dueSaturday.matcher(htmlString);
+        while (m.find()) {
+            returnString = "Yes";
+        }
+        return returnString;
+    }
+
+    private int countBHVars() {
+        int BHVarsCounter = 0;
+        Pattern findvars = Pattern.compile("\\$[A-Za-z]+\\S\\$");
+        Matcher m = findvars.matcher(htmlString);
+        while (m.find()) {
+            BHVarsCounter++;
+        }
+        return BHVarsCounter;
+    }
+    
+    private int numBHLinks() {
+        int bhlinksCounter = 0;
+        bhlinksCounter = links.stream().map((a) -> a.attr("href")).filter((href) -> (href.toLowerCase().contains("brainhoney"))).map((_item) -> 1).reduce(bhlinksCounter, Integer::sum);
+        return bhlinksCounter;
+    }
+
+    private int numBHImages() {
+        int bhimgesCounter = 0;
+        bhimgesCounter = images.stream().map((img) -> img.attr("src")).filter((src) -> (src.toLowerCase().contains("brainhoney"))).map((_item) -> 1).reduce(bhimgesCounter, Integer::sum);
+        return bhimgesCounter;
+    }
+
+    private int numBXLinks() {
+        int bxlinksCounter = 0;
+        bxlinksCounter = links.stream().map((a) -> a.attr("href")).filter((href) -> (href.toLowerCase().contains("box.com"))).map((_item) -> 1).reduce(bxlinksCounter, Integer::sum);
+        return bxlinksCounter;
+    }
+
+    private int numEmptyLinks() {
+        int emptyLinks = 0;
+        for (Element a : links) {
+            boolean noHref = a.attr("href").isEmpty();
+            boolean noLinkText = a.text().isEmpty();
+            boolean hasHref = a.hasAttr("href");
+            if (noHref == true || noLinkText == true || hasHref == false) {
+                emptyLinks++;
+            }
+        }
+        return emptyLinks;
+    }
+
+    private String checkHeaders() {
+        String headers = "";
+        if (!body.getElementsByTag("h1").isEmpty()) {
+            headers += "1";
+        }
+        if (!body.getElementsByTag("h2").isEmpty()) {
+            headers += "2";
+        }
+        if (!body.getElementsByTag("h3").isEmpty()) {
+            headers += "3";
+        }
+        if (!body.getElementsByTag("h4").isEmpty()) {
+            headers += "4";
+        }
+        if (!body.getElementsByTag("h5").isEmpty()) {
+            headers += "5";
+        }
+        if (!body.getElementsByTag("h6").isEmpty()) {
+            headers += "6";
+        }
+        if (headers == "") {
+            headers = "NONE";
+        }
+        if ("123456".indexOf(headers) == 0) {
+            return "Good: " + headers;
+        } else {
+            return "Bad: " + headers;
+        }
+    }
+
+    private int numBadTargets() {
+        int tgCounter = 0;
+        tgCounter = links.stream().filter((a) -> (!a.attr("target").toLowerCase().contains("_blank".toLowerCase()))).map((_item) -> 1).reduce(tgCounter, Integer::sum);
+        return tgCounter;
+    }
+
+    private int numBadImageWidth() {
+        int imgCounter = 0;
+        for (Element img : images) {
+            String width = img.attr("width");
+            if (!width.toLowerCase().contains("%") && !img.attr("src").toLowerCase().contains("banner")) {
+                imgCounter++;
+            }
+        }
+        return imgCounter;
+    }
+
+    private int numDivs() {
+        int divCounter = 0;
+        divCounter = divs.stream().filter((div) -> (!div.hasAttr("id"))).map((_item) -> 1).reduce(divCounter, Integer::sum);
+        return divCounter;
+    }
+
+    private int numBolds() {
+        int bCounter = 0;
+        Pattern dueSaturday = Pattern.compile("font-weight\\: bold");
+        Matcher m = dueSaturday.matcher(htmlString);
+        while (m.find()) {
+            bCounter++;
+        }
+        return bCounter;
+    }
+}
